6544e50335fc58da9a08879db875e697eb4a2ed4
Pratik Kumar <61487131+thekingmass@users.noreply.github.com>
2022-04-29 12:47:31 +0530

    Update Solution.java
    this solution is passing all the test cases

--- SmallestVowelOfString/Solution.java
+++ SmallestVowelOfString/Solution.java
@@ -3,8 +3,14 @@ class MyClass{
  public static void main(String[] args){
    Scanner sc = new Scanner(System.in);
    String st = sc.nextLine();
-   int c = st.charAt(0);
-   //System.out.println(c);
+   int c = 0;
+   // Assigning the first vowel ascii to the variable
+   for(int i=0;i<st.length();i++){
+    if(st.charAt(i)=='a' || st.charAt(i)=='e' || st.charAt(i)=='i' || st.charAt(i)=='o' || st.charAt(i)=='u'){
+     c = st.charAt(i);
+    }
+   }
+//  checking the entire string for the smallest vowel
    for(int i=0;i<st.length();i++){
     if(st.charAt(i)=='a' || st.charAt(i)=='e' || st.charAt(i)=='i' || st.charAt(i)=='o' || st.charAt(i)=='u'){
       int temp = (int)st.charAt(i);
@@ -16,5 +22,4 @@ class MyClass{
    System.out.println((char)c);
    
  }
-}
-            
+}        
